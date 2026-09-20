dfabbe8782d16883bb93a869f8fabd68b849d9d4
MKMithun2806 <mmithunkrish@gmail.com>
2026-09-20 07:15:19 +0000

    core: enable end column output when --vimgrep is used
    Wire the new printer end_column option to activate automatically
    when --vimgrep is passed. This makes the --vimgrep output format:
    
      file:line:startcol:endcol:text
    
    which matches what Vim's errorformat %f:%l:%c:%k:%m expects,
    enabling Vim to populate end_col in quickfix list items instead
    of defaulting to 0.
    
    Refs: https://github.com/BurntSushi/ripgrep/issues/3532

--- crates/core/flags/hiargs.rs
+++ crates/core/flags/hiargs.rs
@@ -625,6 +625,7 @@ impl HiArgs {
             .byte_offset(self.byte_offset)
             .color_specs(self.colors.clone())
             .column(self.column)
+            .end_column(self.vimgrep)
             .heading(self.heading)
             .hyperlink(self.hyperlink_config.clone())
             .max_columns_preview(self.max_columns_preview)
